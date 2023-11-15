38cd7836c3f88b160a3245f973f23f6259881daf
Umbra <69671761+Umbra999@users.noreply.github.com>
2023-11-15 06:55:06 +0100

    Init

---
+++ StarcitizensBypass/Run.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace StarcitizensBypass
+{
+    internal class Run
+    {
+        public static void Main(string[] args)
+        {
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = "Bin64\\StarCitizen.exe",
+                Arguments = string.Join(" ", args)
+            });
+        }
+    }
+}
